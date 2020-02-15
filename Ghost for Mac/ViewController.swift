ac0a7231441b171d84177f067464ad249e09a007
Bezaleel Ashefor <knightbenax@gmail.com>
2020-02-15 23:04:33 +0100

    Initial Commit

---
+++ Ghost for Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ghost for Mac
+//
+//  Created by Bezaleel Ashefor on 15/02/2020.
+//  Copyright © 2020 Ephod. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Ghost for MacTests/Ghost_for_MacTests.swift
@@ -0,0 +1,34 @@
+//
+//  Ghost_for_MacTests.swift
+//  Ghost for MacTests
+//
+//  Created by Bezaleel Ashefor on 15/02/2020.
+//  Copyright © 2020 Ephod. All rights reserved.
+//
+
+import XCTest
+@testable import Ghost_for_Mac
+
+class Ghost_for_MacTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
