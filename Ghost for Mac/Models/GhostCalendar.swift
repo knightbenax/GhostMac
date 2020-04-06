857e95d18b125fb30338addb3a8ab6de3c06b990
Bezaleel Ashefor <knightbenax@gmail.com>
2020-04-06 23:24:14 +0100

    Screenshots Folder

---
+++ Ghost for Mac/Models/GhostCalendar.swift
@@ -0,0 +1,24 @@
+//
+//  GhostCalendar.swift
+//  Ghost
+//
+//  Created by Bezaleel Ashefor on 18/12/2019.
+//  Copyright © 2019 Ephod. All rights reserved.
+//
+
+import Foundation
+
+
+class GhostCalendar{
+    
+    var name : String
+    var id : String
+    
+    init(name: String, id: String) {
+        
+        self.name = name
+        self.id = id
+        
+    }
+    
+}
